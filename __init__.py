4f0f46039452a6e80df30fd623ef2f2f9283b639
WildboarG <mm62633482@gmail.com>
2024-07-20 09:51:24 +0800

    Fixed  How mach page were not found and simplified code

--- __init__.py
+++ __init__.py
@@ -4,15 +4,13 @@ host = "https://bbs.ai-thinker.com"
 um ="/forum.php"
 plugin ="/plugin.php"
 home = host + um
-loginindex= host+"/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login"
 loginurl = host+plugin+"?id=one_sms:api&action=user&inajax=1"
-urlid =host+plugin+"?id=one_sms&action=user_login&referer=https%3A%2F%2Fbbs.ai-thinker.com%2Fforum.php&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login"    
 signhome = host+plugin+"?id=dc_signin:sign&infloat=yes&handlekey=sign&inajax=1&ajaxtarget=fwin_content_sign"
 signrequest = host+plugin+"?id=dc_signin:sign&inajax=1"
-sendurl = host+um+"?mod=post&action=reply&fid=179&tid=45019&extra=&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1"
-urlhuo = host+um+"?mod=viewthread&tid=45019"
+sendaddr = home+"?mod=post&action=reply&fid=179&tid=45019&extra=&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1"
+urlhuo = home+"?mod=viewthread&tid=45019&_dsign=707f90f0"
 
-Username = "xxxxxxx"
+Username = "WildboarG"
 Password = "xxxxxxx"
 uid= "xxxxx"
 
@@ -34,9 +32,7 @@ msg =[
     "祝安信可社区一周年快乐! 一路相伴",
     "祝安信可社区一周年快乐! 大家一起学习",
     "祝安信可社区一周年快乐:lol",
-    "安信可社区一周年快乐! 大佬们带带我",
     "安信可社区一周年快乐! 啦啦啦啦啦",
-    "祝安信可社区一周年快乐！感谢社区大佬们",
     "祝安信可社区一周年快乐，祝安信可社区一周年快乐！",
     "祝安信可社区一周年快乐！祝大家一周年快乐！",
     "祝安信可社区一周年快乐！我又来板砖了",
